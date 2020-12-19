6df4c70436d95571b13bf040bf140a043d32dc71
asepsupriyanto <1910631170166@student.unsika.ac.id>
2020-12-20 01:38:15 +0700

    floating

--- PHPTutorial/PHP5Tutorial/PHP_Numbers/lat.php
+++ PHPTutorial/PHP5Tutorial/PHP_Numbers/lat.php
@@ -22,7 +22,32 @@
     // $x = 1.9e411;
     // var_dump($x);
 
-    
+    //nan
+    // $x = acos(8);
+    // var_dump($x);
+
+    //numerical strings
+    // $x = 5985;
+    // var_dump(is_numeric($x));
+
+    // $x = "5985";
+    // var_dump(is_numeric($x));
+
+    // $x = "59.85" + 100;
+    // var_dump(is_numeric($x));
+
+    // $x = "Hello";
+    // var_dump(is_numeric($x));
+
+    $x = 23465.768;
+    $int_cast = (int)$x;
+    echo $int_cast;
+
+    echo "<br>";
+
+    $x = "23465.768";
+    $int_cast = (int)$x;
+    echo $int_cast;
     ?>
 </body>
 </html>
