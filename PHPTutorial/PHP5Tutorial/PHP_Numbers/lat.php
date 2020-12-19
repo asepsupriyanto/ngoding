e9bca9d010642ed6228ebd0ced151067c879e673
asepsupriyanto <1910631170166@student.unsika.ac.id>
2020-12-20 01:27:39 +0700

    ngoding

--- PHPTutorial/PHP5Tutorial/PHP_Numbers/lat.php
+++ PHPTutorial/PHP5Tutorial/PHP_Numbers/lat.php
@@ -7,11 +7,22 @@
 </head>
 <body>
     <?php
-    $x = 5985;
-    var_dump(is_int($x));
+    // integer
+    // $x = 5985;
+    // var_dump(is_int($x));
 
-    $x = 59.85;
-    var_dump(is_int($x));
+    // $x = 59.85;
+    // var_dump(is_int($x));
+
+    //float
+    // $x = 10.365;
+    // var_dump(is_float($x));
+
+    //infinite
+    // $x = 1.9e411;
+    // var_dump($x);
+
+    
     ?>
 </body>
 </html>
