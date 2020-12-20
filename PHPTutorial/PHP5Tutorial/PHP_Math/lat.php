714ad08bd4fd5f63dbc08abceb3257ce51c83668
asepsupriyanto <1910631170166@student.unsika.ac.id>
2020-12-20 17:00:36 +0700

    PHP_math

---
+++ PHPTutorial/PHP5Tutorial/PHP_Math/lat.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+ <?php
+  echo(min(0, 150, 30, 20, -8, -200) . "<br>");
+  echo(max(0, 150, 30, 20, -8, -200). "<br>");
+  echo(abs(-6.7). "<br>");
+  echo(sqrt(64)). "<br>";
+  echo(rand(). "<br>");
+  echo(rand(10, 100));
+?>
+  </body>
+</html>
