b74f67ea9554cfb1cd98c264383f2939061a6a15
asepsupriyanto <1910631170166@student.unsika.ac.id>
2020-12-22 18:09:27 +0700

    loop

---
+++ PHPTutorial/PHP5Tutorial/PHP_Loop/lat.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // while
+    // $x = 5;
+
+    // while($x <= 100){
+    //     echo "Nomor $x<br>";
+    //     $x+=5;
+    // }
+
+    //do while
+    $x = 6;
+    do {
+        echo "Nomor $x<br>";
+        $x++;
+    }while ($x <=5);
+
+
+
+    ?>
+</body>
+</html>
