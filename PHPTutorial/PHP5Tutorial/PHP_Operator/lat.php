57bc8b341cc12fa0261cd6b0dc59a26af27101a9
asepsupriyanto <1910631170166@student.unsika.ac.id>
2020-12-22 16:20:34 +0700

    operator

---
+++ PHPTutorial/PHP5Tutorial/PHP_Operator/lat.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AS</title>
+</head>
+<body>
+  <?php
+    $x = 20;
+    $x -=100;
+
+    echo $x;
+    ?>
+</body>
+</html>
