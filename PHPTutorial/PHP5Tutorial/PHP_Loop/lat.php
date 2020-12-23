1e81cb6955c94bc9016042d45d872c981a583d57
asepsupriyanto <1910631170166@student.unsika.ac.id>
2020-12-23 14:57:31 +0700

    loop

--- PHPTutorial/PHP5Tutorial/PHP_Loop/lat.php
+++ PHPTutorial/PHP5Tutorial/PHP_Loop/lat.php
@@ -16,14 +16,22 @@
     // }
 
     //do while
-    $x = 6;
-    do {
-        echo "Nomor $x<br>";
-        $x++;
-    }while ($x <=5);
+    // $x = 6;
+    // do {
+    //     echo "Nomor $x<br>";
+    //     $x++;
+    // }while ($x <=5);
+    
+    //for loop
+    // for ($x = 0; $x <= 100; $x+=10) {
+    //     echo "Nomor $x <br>";
+    // }
 
+    $nama = array("asep"=>"20", "supri"=>"19", "ahmad"=>"6","faris"=>"6");
 
-
+    foreach ($nama as $x => $val) {
+        echo "$x = $val<br>";
+    }
     ?>
 </body>
 </html>
