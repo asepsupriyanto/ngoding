579eaecca3c4bd2d3062a545b8f206a44cc0837b
asepsupriyanto <1910631170166@student.unsika.ac.id>
2021-01-26 15:03:17 +0700

    variabe;

---
+++ PHPAdvanced/include.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="menu">
+    <?php include 'vars.php';?>
+     </div>
+    <h1>Welcome to my home page</h1>
+    <p>Beberapa text</p>
+    <p>Beberapa text</p>
+
+    <?php include 'vars.php';
+    echo "I have a $color $car.";
+    ?>
+</body>
+</html>
---
+++ PHPAdvanced/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo '<a href="/default.asp">Home</a> - 
+            <a href="html/default.asp">HTML Tutorial</a>-
+            <a href="/css/default.asp">CSS Tutorial</a> -
+            <a href="/js/default.asp"JavaScript Tutorial</a>-
+            <a href="default.asp">PHP Tutorial</a>
+            
+            ' ;
+    ?>
+
+</body>
+</html>
